1fef13c4be5e9b76a8a77f90b29fae5cdd8a4d2a
Bakai Taalaibek <bakaitaalaibek@gmail.com>
2017-02-21 19:01:56 +0600

    fixed js turbolinks

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,11 +1,13 @@
-jQuery.fn.submitOnCheck = 	function(){
-	this.find('input[type=submit]').remove();
-	this.find('input[type=checkbox]').click(function(){
-		$(this).parent('form').submit();
-	});
-	return this;
-};
+$(document).on('turbolinks:load', function() {
+		jQuery.fn.submitOnCheck = function(){
+		this.find('input[type=submit]').remove();
+		this.find('input[type=checkbox]').click(function(){
+			$(this).parent('form').submit();
+		});
+		return this;
+	};
 
-$(function(){
-	$('.edit_todo').submitOnCheck();
+	$(function(){
+		$('.edit_todo').submitOnCheck();
+	});
 });
